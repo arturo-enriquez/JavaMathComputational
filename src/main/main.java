f0cf5e0a801a57fd5295e65285d72f9d4e017e95
Arturo Enriquez <arturoccompany@gmail.com>
2019-04-18 21:06:52 -0700

    Primer commit

---
+++ src/main/main.java
@@ -0,0 +1,17 @@
+package main;
+
+import interfaces.index;
+
+public class main {
+
+    public static void main(String[] args) {
+        
+        
+        index index = new index();
+        
+        index.setVisible(true);
+        
+        
+    }
+    
+}
